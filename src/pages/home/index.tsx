d0a4ea51f9691c7474a6de62701ad817eb5972a7
Kelly Galliani <kelly_92b@hotmail.com>
2024-10-15 11:10:58 -0300

    link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,7 +30,7 @@ import { useLanguage } from "../../providers/languageContext";
 
 export const Home = (): JSX.Element => {
   const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/portfolio`;
+  const portfolioUrl = `https://github.com/${userData.githubUser}/portifolio`;
 
   const { language } = useLanguage();
 
